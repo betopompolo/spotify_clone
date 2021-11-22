c7c53afbd6c08a0ada22907166c5adbe23d5ba97
Adalberto Pompolo <adalberto.pompolo@taqtile.com.br>
2021-11-22 15:01:18 -0300

    fix: remove Stack (FloatingMusicPlayer)

--- lib/src/music_player/floating_music_player.dart
+++ lib/src/music_player/floating_music_player.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spotify_clone/src/common/models.dart';
 import 'package:spotify_clone/src/music_player/music_player.provider.dart';
 
-// TODO: refactor without Stack
 class FloatingMusicPlayer extends ConsumerWidget {
   const FloatingMusicPlayer({Key? key}) : super(key: key);
 
@@ -19,56 +18,49 @@ class FloatingMusicPlayer extends ConsumerWidget {
       ),
       margin: const EdgeInsets.all(8.0),
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(4.0),
-                      child: Image.network(
-                        currentAlbum.thumbnailUrl,
-                        height: 32,
-                        width: 32,
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            currentMusic.name,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                          Text(
-                            currentAlbum.title,
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const PlayPauseButton(),
-                  ],
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4.0),
+                  child: Image.network(
+                    currentAlbum.thumbnailUrl,
+                    height: 32,
+                    width: 32,
+                  ),
                 ),
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        currentMusic.name,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Text(
+                        currentAlbum.title,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ),
+                const PlayPauseButton(),
               ],
             ),
-          ),
-          Positioned(
-            child: Container(
-              height: 2.0,
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: MusicProgressIndicator(currentMusic),
+            const SizedBox(
+              height: 8,
             ),
-            bottom: 0,
-            left: 0,
-            right: 0,
-          ),
-        ],
+            SizedBox(
+              height: 2.0,
+              child: MusicProgressIndicator(currentMusic),
+            )
+          ],
+        ),
       ),
     );
   }
